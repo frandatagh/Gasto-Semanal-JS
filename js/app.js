38e0e985113c4623f2a3376d691cbc94b80b3912
Fran Ruiz <francoruizgabriel@gmail.com>
2023-10-25 20:22:57 -0300

    Validación de formulario

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ const gastoListado = document.querySelector("#gastos ul");
 eventListeners();
 function eventListeners() {
   document.addEventListener("DOMContentLoaded", preguntarPresupuesto);
+
+  formulario.addEventListener("submit", agregarGasto);
 }
 
 // Classes
@@ -26,6 +28,30 @@ class UI {
     // Agregar al HTML
     document.querySelector("#total").textContent = presupuesto;
     document.querySelector("#restante").textContent = restante;
+  }
+
+  // Imprimir datos
+  imprimirAlerta(mensaje, tipo) {
+    //crear el div
+    const divMensaje = document.createElement("div");
+    divMensaje.classList.add("text-center", "alert");
+
+    if (tipo === "error") {
+      divMensaje.classList.add("alert-danger");
+    } else {
+      divMensaje.classList.add("alert-success");
+    }
+
+    // Mensaje de error
+    divMensaje.textContent = mensaje;
+
+    // Insertar en el HTML
+    document.querySelector(".primario").insertBefore(divMensaje, formulario);
+
+    // Quitar del HTML
+    setTimeout(() => {
+      divMensaje.remove();
+    }, 3000);
   }
 }
 
@@ -54,4 +80,25 @@ function preguntarPresupuesto() {
   console.log(presupuesto);
 
   ui.insertarPresupuesto(presupuesto);
+}
+
+//Añade gastos
+
+function agregarGasto(e) {
+  e.preventDefault();
+
+  // Leer los datos del formulario
+  const nombre = document.querySelector("#gasto").value;
+  const cantidad = document.querySelector("#cantidad").value;
+
+  //Validar
+  if (nombre === "" || cantidad === "") {
+    ui.imprimirAlerta("Ambos campos son obligatorios", "error");
+    return;
+  } else if (cantidad <= 0 || isNaN(cantidad)) {
+    ui.imprimirAlerta("Cantidad no válida", "error");
+    return;
+  }
+
+  console.log("Agregando Gasto");
 }
